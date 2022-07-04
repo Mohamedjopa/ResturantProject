fb30d9c2728d162e8fbe13a524e8e6d36920c7d9
Mohamed hassan <104630281+Mohamedjopa@users.noreply.github.com>
2022-07-05 01:21:07 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+create table employee
+(
+Email varchar(40),
+password varchar(15)
+)
+insert into employee values('moh@gmail.com','1234')
+insert into employee values('ahm@gmail.com','5555')
+insert into employee values('ali@gmail.com','4321')
